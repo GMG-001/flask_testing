c769127404dbaf00ecb9ecccee117b7d7fd0dca8
giorgi megrelishvili <giorgi@localhost.localdomain>
2020-09-23 10:46:57 +0400

    test

--- weather_flask/app.py
+++ weather_flask/app.py
@@ -1,16 +1,14 @@
-import requests
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # url='api.openweathermap.org/data/2.5/weather?q={}&appid={your api key}'
-    # city='Las Vegas'
-    # r=requests.get(url.format(city)).json
 
+@app.route('/')
+def home():
     return render_template('weather.html')
+
+
+
 
 
 if __name__=='__main__':
