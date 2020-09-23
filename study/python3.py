c1c6e9e1c48f50ef8dba99929870fc1dceee359d
giorgi megrelishvili <giorgi@localhost.localdomain>
2020-09-23 10:39:49 +0400

    test

---
+++ study/python3.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # url='api.openweathermap.org/data/2.5/weather?q={}&appid={your api key}'
+    # city='Las Vegas'
+    # r=requests.get(url.format(city)).json
+
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
